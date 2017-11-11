20f6008e1c200bd3b77a199d4334f1742b1d4cc8
ShreyesBhat <32994823+ShreyesBhat@users.noreply.github.com>
2017-11-11 23:14:30 +0530

    Update TelephoneDirectory.c

--- TelephoneDirectory.c
+++ TelephoneDirectory.c
@@ -1,1 +1,25 @@
-
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+typedef struct
+{
+    char n[20] = " ";
+} names;
+typedef struct 
+{ 
+    names name[4];
+    long int tel;
+    int numocc; 
+} entry;
+entry e[10];
+void create()
+{
+     strcpy(e[0].name[0].n, "Mahesh");
+     e[0].tel=9722148381; if(!e[0].name[2].n) puts(e[0].name[0].n);
+}
+int main()
+{
+    create();
+    
+    return 0;
+}
